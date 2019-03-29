8ef4cd2f3b0e4961875015a193bdebf18b32fba1
amit15277 <48946259+amit15277@users.noreply.github.com>
2019-03-29 16:03:29 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  access_key = "AKIAJGVKRAMFEYJXNHHA"
+  secret_key = "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"
+  region     = "ap-south-1"
+}
+
+#variable "count" {
+#default=2
+#}
+
+resource "aws_instance" "Amit1" {
+#  count = "${var.count}"
+  ami           = "ami-03291442a55ad02c1"
+  instance_type = "t2.micro"
+}
+
+resource "aws_ebs_volume" "volume" {
+    availability_zone = "ap-south-1a"
+    encrypted = false
+    iops = 500
+    size = 250
+    type = "gp2"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+
+  tags {
+   Name = "myvpc"
+}
+
+}
