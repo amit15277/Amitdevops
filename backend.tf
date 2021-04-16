5c5ba797c99f0dfc7439a09f81b1d3434c36b9eb
amit15277 <48946259+amit15277@users.noreply.github.com>
2021-04-16 21:39:27 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudlogic20-tfstate"
-    key            = "terraformdemo.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "s3-state-lock"
-  }
-}
